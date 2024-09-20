1e73bb321aca5e0a274122c32c7a2fcfc5d91908
omprakash1907 <omprakashkjat19@gmail.com>
2024-09-21 02:36:12 +0530

    db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,20 @@
-const mongoose = require('mongoose')
-const Config = require('.')
+const mongoose = require('mongoose');
+const Config = require('.');
 
-const url = Config.MONGO_URI
+const url = Config.MONGO_URI;
 
 const connectDB = async () => {
-    try{
-        await mongoose.connect(url)
-        console.log("Database Connected 🚀🚀")
-    }catch(err){
-        console.log(err)
+    try {
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000,  // Timeout after 5s if MongoDB server isn't found
+            socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+        });
+        console.log('Database Connected 🚀🚀');
+    } catch (err) {
+        console.error('Error connecting to the database:', err);
     }
-}
+};
 
 module.exports = connectDB;
